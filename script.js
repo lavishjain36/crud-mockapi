ab15828a14c81ab589cf679e7efb3a633360596e
Lavish Jain <lavishjain36@gmail.com>
2021-12-18 12:24:06 +0530

    Callback Hell

--- script.js
+++ script.js
@@ -1,35 +1,21 @@
-//Buil an html elements
-document.body.innerHTML = `<div class="heading-container">
-<h1>Brewerys List</h1>
-<img class="icon" src="https://static.thenounproject.com/png/2559187-200.png" width="50px" height="50px"alt="icon">
+const countdown = document.querySelector(".countdown");
+let time = 5;
 
-</div>
-<div id="mainContainer" class="main-container"> </div>`;
+countdown.innerText = time--;
+setTimeout(() => {
+  countdown.innerText = time--;
+  setTimeout(() => {
+    countdown.innerText = time--;
+    setTimeout(() => {
+      countdown.innerText = time--;
+      setTimeout(() => {
+        countdown.innerText = time--;
+        setTimeout(() => {
+          countdown.innerText = "Happy X-mas";
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+}, 1000);
 
-const getData = async () => {
-  try {
-    const data = await fetch("https://api.openbrewerydb.org/breweries");
-    const brewerys = await data.json();
-    mainContainer.innerHTML = "";
-    brewerys.forEach((brewery) => {
-      displayData(brewery);
-    });
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-getData();
-const displayData = (obj) => {
-  mainContainer.innerHTML += `
-  <div class="container">
-  <h3 class="blue">Breweries Name:<span>${obj.city}</span></h3>
-  <p class="para blue">Brewries Type:<span>${obj.brewery_type}</span></p>
-  <p class="para blue">Brewries Phone:<span>${obj.phone}</span></p>
-  <p class="para blue">Brewries Website:<span>${obj.website_url}</span></p>
-  <p class="para blue">Brewries Address:<span>${obj.street}</span></p>
-  <p class="para blue">Brewries State:<span>${obj.state}</span></p>
-  <p class="para blue">Brewries Zip:<span>${obj.postal_code}</span></p>
-  <p class="para blue">Brewries Country:<span>${obj.country}</span></p>
-  </div>`;
-};
+// Callback =>Pain of callback and (Callback hell)
